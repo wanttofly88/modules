908096e50f30ecd9687e38f9994735804689ea36
wanttofly88 <wanttofly88@gmail.com>
2016-02-21 19:29:24 +0300

    firefox hotfix

--- modules/form/form-focus.view.js
+++ modules/form/form-focus.view.js
@@ -11,6 +11,12 @@ define(['dispatcher'], function(dispatcher) {
 	var _add = function(items, element) {
 		var id = element.getAttribute('data-id');
 
+		if (element.value) {
+			element.parentNode.classList.add('not-empty');
+		} else {
+			element.parentNode.classList.remove('not-empty');
+		}
+
 		element.addEventListener('focus', function() {
 			element.parentNode.classList.remove('error');
 			element.parentNode.classList.add('focus');
